7295324301a46c4eea931cc6b0636d67486b9375
Franulo <franciszek@balamut.eu>
2023-10-14 18:15:45 +0200

    commit

--- App.js
+++ App.js
@@ -15,9 +15,10 @@ export default function App() {
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subtitle}>Starting at $69,420</Text>
-          
         </View>
+
       </View>
+      
     </View>
   );
 }
